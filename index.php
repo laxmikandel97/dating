751004a6b95bf07fded2b0bfafe089bf40a5de7c
Laxmi Kandel <laxmikandel97@gmail.com>
2020-01-16 02:26:35 -0500

    Defined a default route that renders a views/home.html and some minor Grammar fix

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+//THIS IS OUR CONTROLLER
+
+//Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//require the autoloaded file
+require_once ('vendor/autoload.php');
+//create an instance of the base class
+$f3= Base::instance();
+
+//Define a default route
+$f3->route('GET /', function (){
+    $view = new Template();
+    echo $view-> render('views/home.html');
+});
+
+//run fat free
+$f3->run();
+
