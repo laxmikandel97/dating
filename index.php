4a55c351536bcde7c38bcb66f989f257c2900a80
Laxmi Kandel <laxmikandel97@gmail.com>
2020-01-25 02:09:28 -0500

    Set default route for all the forms pages and got the user input value and store in seesion and displayed it in summary page

--- index.php
+++ index.php
@@ -1,36 +1,50 @@
-<!--Laxmi Kandel-->
-<!--January 16 2020-->
-<!--Setting default route for dating site-->
 <?php
+session_start();
 //THIS IS OUR CONTROLLER
-
 //Turn on error reporting
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
+//session_start();
 //require the autoloaded file
-require_once ('vendor/autoload.php');
+require_once('vendor/autoload.php');
 //create an instance of the base class
-$f3= Base::instance();
-
+$f3 = Base::instance();
 //Define a default route
-$f3->route('GET /', function (){
+$f3->route('GET /', function () {
     $view = new Template();
-    echo $view-> render('views/home.html');
+    echo $view->render('views/home.html');
 });
 //default route for personalInfo page
-$f3->route('GET /personalInfo', function (){
+$f3->route('GET /personalInfo', function () {
     $view = new Template();
-    echo $view-> render('views/personalInfo.html');
+    echo $view->render('views/personalInfo.html');
 });
-
 //default route for profile page
-$f3->route('GET /profile', function (){
+$f3->route('POST /profile', function () {
+    $_SESSION['firstName'] = $_POST['firstName'];
+    $_SESSION['lastName'] = $_POST['lastName'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['phone'] = $_POST['phone'];
     $view = new Template();
-    echo $view-> render('views/profile.html');
+    echo $view->render('views/profile.html');
 });
-
-
+$f3->route('POST /interests', function () {
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['seeking'] = $_POST['seeking'];
+    $_SESSION['inputText'] = $_POST['inputText'];
+    $view = new Template();
+    echo $view->render('views/interests.html');
+});
+$f3->route('POST /summary', function () {
+    $interest=$_POST['interests'];
+    $_SESSION['values']=array();
+    foreach ($interest as $item) {
+        array_push($_SESSION['values'],$item);
+    }
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
 //run fat free
 $f3->run();
-
